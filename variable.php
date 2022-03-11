2e0d1c264aed7a759a901f403f14db1b0bfd9a46
DwiAsfi <dwiasfi2@gmail.com>
2022-03-11 11:37:48 +0700

    Menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+$a = 11;
+$b = 20;
+$c = 3;
+$d = 5;
+
+$penjumlahan = $a + $c;
+$pengurangan = $b - $a;
+$perkalian = $d * $c;
+$pembagian = $b / $d;
+
+echo "11 + 3 = $penjumlahan </br>";
+echo "20 - 11 = $pengurangan </br>";
+echo "5 * 3 = $perkalian </br>";
+echo "20 / 5 = $pembagian </br>";
+?>
